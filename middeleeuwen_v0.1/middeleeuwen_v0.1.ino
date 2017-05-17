113aafb16ef8c3c55b08b4e92c6f17f3358d189f
Floris-Laptop <0928136@hr.nl>
2017-05-18 00:04:55 +0200

    -Tried making it work, code still a mess.

--- middeleeuwen_v0.1/middeleeuwen_v0.1.ino
+++ middeleeuwen_v0.1/middeleeuwen_v0.1.ino
@@ -3,17 +3,26 @@
  */
  
 //A total of 8 handles
-int handlePins = [1,2,3,4,5,6,7,8];
+int handlePins[] = {2,3,4,5,6,7,8,9};
 
 //These handles need to be pulled down to have a correct code (the numbers point to the handlePorts Array).
-bool rightHandles = [false,true,false,true,false,true,false,true];
+bool rightHandles[] = {false,true,false,true,false,true,false,true};
+
+//Variable to store wether the handles are completed
+bool handlesCompleted = false;
 
 void setup() {
   setupHandles();
+  Serial.begin(9600);
 }
 
 void loop() {
-  
+  if(!handlesCompleted){
+    if(checkHandles()){
+      handlesCompleted = true;
+      Serial.println("You did it");
+    }
+  }
 
 }
 
@@ -22,7 +31,7 @@ void loop() {
  */
 void setupHandles(){
   int arrayLength = sizeof(handlePins)/sizeof(int);
-  for(int i = 0; i < arraylength; i++){
+  for(int i = 0; i < arrayLength; i++){
     pinMode(handlePins[i], INPUT);
   }
 }
@@ -34,18 +43,22 @@ void setupHandles(){
 bool checkHandles(){
   int arrayLengthHandles = sizeof(handlePins)/sizeof(int);
   int totalCorrect = 0;
-  for(int i = 0; i < arrayLenghtHandles; i++){
+  for(int i = 0; i < arrayLengthHandles; i++){
     if(rightHandles[i]){
       if(digitalRead(handlePins[i]) == HIGH){
+        String correct = "Pin: " + i;;
+        Serial.println(correct);
         totalCorrect++;
       }
     }else{
       if(digitalRead(handlePins[i]) == LOW){
-        totalCorrect++
+        String correct = "Pin: " + i;;
+        Serial.println(correct);
+        totalCorrect++;
       }
     }
   }
-  if(totalCorrect == 8){
+  if(totalCorrect == 8){  
     return true;
   }else{
     return false;
