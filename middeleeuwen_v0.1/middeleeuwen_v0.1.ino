3360ec2af693fad799383c2c0a8e0dfead6eacfd
Floris-Laptop <0928136@hr.nl>
2017-05-24 13:35:19 +0200

    -Went from 8 levers to 6 levers

--- middeleeuwen_v0.1/middeleeuwen_v0.1.ino
+++ middeleeuwen_v0.1/middeleeuwen_v0.1.ino
@@ -1,12 +1,15 @@
 /**
  * Global Variables
  */
- 
+//Timing stuff
+unsigned long currentTime;
+
+
 //A total of 8 handles
-int handlePins[] = {2,3,4,5,6,7,8,9};
+int handlePins[] = {2,3,4,5,6,7};
 
 //These handles need to be pulled down to have a correct code (the numbers point to the handlePorts Array).
-bool rightHandles[] = {false,true,false,true,false,true,false,true};
+bool rightHandles[] = {false,true,false,false,true,true};
 
 //Variable to store wether the handles are completed
 bool handlesCompleted = false;
@@ -17,12 +20,29 @@ void setup() {
 }
 
 void loop() {
+  //Set the current time
+  currentTime = millis();
+
+  //Check if handles are Completed
   if(!handlesCompleted){
+    
+    //Check if handles are correct
     if(checkHandles()){
       handlesCompleted = true;
       Serial.println("You did it");
+    }else{
+      
     }
+  }else{
+    //Handles are correct.
+    
   }
+
+  //Check if sword is inserted
+
+
+  //Check if sword was inserted and handler are completed
+    //Open door
 
 }
 
@@ -46,22 +66,17 @@ bool checkHandles(){
   for(int i = 0; i < arrayLengthHandles; i++){
     if(rightHandles[i]){
       if(digitalRead(handlePins[i]) == HIGH){
-        String correct = "Pin: " + i;;
-        Serial.println(correct);
         totalCorrect++;
       }
     }else{
       if(digitalRead(handlePins[i]) == LOW){
-        String correct = "Pin: " + i;;
-        Serial.println(correct);
         totalCorrect++;
       }
     }
   }
-  if(totalCorrect == 8){  
+  if(totalCorrect == arrayLenghtHandles){  
     return true;
   }else{
     return false;
   }
 }
-
