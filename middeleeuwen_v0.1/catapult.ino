0a77b260ef99ae5930e51f5eda465489c092384b
Michael Kool <michael10mail@gmail.com>
2017-05-24 15:32:18 +0200

    added voids

--- middeleeuwen_v0.1/catapult.ino
+++ middeleeuwen_v0.1/catapult.ino
@@ -1,11 +1,17 @@
 /**
 * comment
 */
-
-//global variables
+void setup(){
+  //global variables
 bool targetHit = true;
+
+}
+
+void loop(){
 //if target is hit show number
 if(targetHit){
    Serial.println("Je hebt de target geraakt. Code cijfer: 7"); 
+  
+  }
 }
 
